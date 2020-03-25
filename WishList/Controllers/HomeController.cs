48547b29f2dff75f56ae8b51ea31add7ec21dc3d
Ugljesa Cubrilo <ugljesacubrilo@gmail.com>
2020-03-25 22:33:02 +0100

    Action fix 2

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,12 +10,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View("./Index");
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return View("./Error");
+            return View("Error");
         }
     }
 }

2b223beb923daf355a61f352c9a09a47cd25b04b
Ugljesa Cubrilo <ugljesacubrilo@gmail.com>
2020-03-25 22:48:05 +0100

    Folders refactor

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WishList.Model;
 
-namespace WishList.Model
+namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
